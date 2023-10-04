761bf4f00cde2247db550087cb9fb2b4dcaabb50
TheTrueYasuo <josuehernandez0613@gmail.com>
2023-10-04 00:14:14 -0700

    Js done

--- game.js
+++ game.js
@@ -1,58 +1,58 @@
-const questions = [
+let questions = [
     {
         question: 'Who is the boy who lived?',
         choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
         answer: 2 
     },
     {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
+        question: 'Who is the headmaster at Hogwarts?',
+        choices: ['Severus Snape', 'Rubeus Hagrid', 'Dobby', 'Albus Dumbledore'],
+        answer: 4 
+    },
+    {
+        question: 'What kind of pet does Ron have?',
+        choices: ['Rat', 'Owl', 'Cat', 'Dog'],
+        answer: 1 
+    },
+    {
+        question: 'What spell is used to fight dementors',
+        choices: ['Expelliarmus', 'Accio', 'Alohomora', 'Patronus'],
+        answer: 4 
+    },
+    {
+        question: 'What is Lord Voldemort real name?',
+        choices: ['Salazar Sletherine', 'Voldemort', 'Tom Riddle', 'Neville Longbottom'],
+        answer: 3 
+    },
+    {
+        question: 'How many horcruxes did veldermot create in total?',
+        choices: ['5', '6', '8', '7'],
+        answer: 3 
+    },
+    {
+        question: 'Who is the half blood prince?',
+        choices: ['Severus Snape', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
+        answer: 1 
+    },
+    {
+        question: 'Which potions allows the person to transfrom appearence?',
+        choices: ['Felix Felicis', 'Polyjuice potion', 'Skele-Gro', 'Wolfsbane potion'],
         answer: 2 
     },
     {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
+        question: 'Who is Harry Potter god father?',
+        choices: ['Albus Dumbledore', 'Severus Snape', 'Sirus Black', 'Hagrid'],
+        answer: 3 
     },
     {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
+        question: 'Who opended the chamber of secrets ?',
+        choices: ['Ginny Weasley', 'Harry Potter', 'Draco Malfoy', 'Hagrid'],
+        answer: 1 
     },
     {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
-        answer: 2 
+        question: 'What is Consider a deadly hollow?',
+        choices: ['Elder Wand', 'The Reserruction Stone', 'The Cloak of Invisibilty', 'All of the above'],
+        answer: 4
     },
 ];
 
