2ebc05e53c665d077ee97ddb4e534603fba67523
TheTrueYasuo <josuehernandez0613@gmail.com>
2023-10-03 21:28:42 -0700

    s

--- game.js
+++ game.js
@@ -1,159 +1,85 @@
-const question = document.querySelector('#questions');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
-const progressBarFull = document.querySelector('#progressBarFull');
+const questions = [
+    {
+        question: 'Who is the boy who lived?',
+        choices: ['Ron Weasley', 'Harry Potter', 'Draco Malfoy', 'Neville Longbottom'],
+        answer: 1 // Index of the correct answer in the choices array
+    },
+    // ... (other questions)
+];
 
-let currentQuestion ={}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+const SCORE_POINTS = 10;
+const MAX_QUESTIONS = 10;
 
-let questions = [
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    },
-    {
-        question: 'Who is the boy who lived?',
-        choice1: 'Ron Weasley',
-        choice2: 'Harry Potter',
-        choice3: 'Draco Malfoy',
-        choice4: 'Neville Longbottom',
-        answer: 2,
-    }
+const questionElement = document.querySelector('#question');
+const choiceElements = Array.from(document.querySelectorAll('.choice-text'));
+const progressTextElement = document.querySelector('#progressText');
+const scoreTextElement = document.querySelector('#score');
+const progressBarFullElement = document.querySelector('#progressBarFull');
 
-]
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
-
-const SCORE_POINTS = 10
-const MAX_QUESTIONS = 10
-
-startGame = () => {
+const startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
 };
 
-getNewQuestion = () => {
+const getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         return window.location.assign('/end.html');
     }
 
     questionCounter++;
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    progressTextElement.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFullElement.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionsIndex];
-    question.innerText = currentQuestion.question;
+    questionElement.innerText = currentQuestion.question;
 
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+    choiceElements.forEach((choice, index) => {
+        choice.innerText = currentQuestion.choices[index];
     });
 
     availableQuestions.splice(questionsIndex, 1);
     acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if (!acceptingAnswers) return;
+const handleChoiceClick = e => {
+    if (!acceptingAnswers) return;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset.number;
 
-        let classToApply = selectedAnswer === currentQuestion.answer ? 'correct' : 'incorrect';
+    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
-        if (classToApply === 'correct') {
-            incrementScore(SCORE_POINTS);
-        }
+    if (classToApply === 'correct') {
+        incrementScore(SCORE_POINTS);
+    }
 
-        selectedChoice.parentElement.classList.add(classToApply);
+    selectedChoice.parentElement.classList.add(classToApply);
 
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
-    });
-});
-
-incrementScore = num => {
-    score += num;
-    scoreText.innerText = score;
+    setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+    }, 1000);
 };
+
+const incrementScore = num => {
+    score += num;
+    scoreTextElement.innerText = score;
+};
+
+// Event listeners for choice clicks
+choiceElements.forEach(choice => {
+    choice.addEventListener('click', handleChoiceClick);
+});
 
 startGame();
