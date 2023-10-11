94d1d73c20ef27cf2112da2664322cbb1a1fbf37
TheTrueYasuo <josuehernandez0613@gmail.com>
2023-10-10 18:40:57 -0700

    doneee

--- game.js
+++ game.js
@@ -81,7 +81,7 @@ const startGame = () => {
 const getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
-        return window.location.assign('/end.html');
+        return window.location.assign('end.html');
     }
 
     questionCounter++;
